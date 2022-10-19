893d8ec114f1babcb1e2341144bf6012df480bed
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-10-19 22:18:43 +0300

    BaseEntity init.

---
+++ core/src/main/java/com/example/eventservice/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.example.eventservice.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuffer("id=")
+                .append(id)
+                .append(", ")
+                .toString();
+    }
+}
