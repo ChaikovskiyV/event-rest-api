5b1379b33361b9c5d943a7e04a2df66c9494bdd4
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-10-23 12:24:31 +0300

    OrganizerDto commit, rename fields, add the toString method.

--- core/src/main/java/com/example/eventservice/dto/OrganizerDto.java
+++ core/src/main/java/com/example/eventservice/dto/OrganizerDto.java
@@ -6,48 +6,61 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-public class OrganizerDto {
+public class OrganizerDto implements BaseDto {
     @NotEmpty(message = "An organizer name can't be empty.")
     @Length(min = 3, max = 20, message = "An organizer name length has to be from 3 to 20 symbols.")
     @Pattern(regexp = "\\w*", message = "An organizer name has to contain only letters.")
-    private String name;
+    private String organizerName;
     @NotEmpty(message = "Email can't be empty.")
     @Email(message = "Email has to be like xxx@xxx.xxx.")
-    private String email;
+    private String organizerEmail;
     @NotEmpty(message = "A telephone number can't be empty.")
     @Pattern(regexp = "\\+\\d{12}", message = "A telephone number has to include plus(+) and 12 digits.")
-    private String telephoneNumber;
+    private String organizerTelephoneNumber;
 
     public OrganizerDto() {
     }
 
-    public OrganizerDto(String name, String email, String telephoneNumber) {
-        this.name = name;
-        this.email = email;
-        this.telephoneNumber = telephoneNumber;
+    public OrganizerDto(String organizerName, String organizerEmail, String organizerTelephoneNumber) {
+        this.organizerName = organizerName;
+        this.organizerEmail = organizerEmail;
+        this.organizerTelephoneNumber = organizerTelephoneNumber;
     }
 
-    public String getName() {
-        return name;
+    public String getOrganizerName() {
+        return organizerName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setOrganizerName(String organizerName) {
+        this.organizerName = organizerName;
     }
 
-    public String getEmail() {
-        return email;
+    public String getOrganizerEmail() {
+        return organizerEmail;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setOrganizerEmail(String organizerEmail) {
+        this.organizerEmail = organizerEmail;
     }
 
-    public String getTelephoneNumber() {
-        return telephoneNumber;
+    public String getOrganizerTelephoneNumber() {
+        return organizerTelephoneNumber;
     }
 
-    public void setTelephoneNumber(String telephoneNumber) {
-        this.telephoneNumber = telephoneNumber;
+    public void setOrganizerTelephoneNumber(String organizerTelephoneNumber) {
+        this.organizerTelephoneNumber = organizerTelephoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuffer("OrganizerDto{")
+                .append("name='")
+                .append(organizerName)
+                .append("', email='")
+                .append(organizerEmail)
+                .append("', organizerTelephoneNumber='")
+                .append(organizerTelephoneNumber)
+                .append("'}")
+                .toString();
     }
 }
