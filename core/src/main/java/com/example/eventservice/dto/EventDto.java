f34df9c8dad6d2ad3e735d672401a75c3773cade
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-10-23 12:21:13 +0300

    EventDto commit, rename fields, replace List<OrganizerDto> to OrganizerDto, add the toString method, change pattern for the eventDate field.

--- core/src/main/java/com/example/eventservice/dto/EventDto.java
+++ core/src/main/java/com/example/eventservice/dto/EventDto.java
@@ -1,65 +1,61 @@
 package com.example.eventservice.dto;
 
-import com.example.eventservice.entity.Address;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.ArrayList;
-import java.util.List;
 
-public class EventDto {
+public class EventDto implements BaseDto {
     @NotEmpty(message = "A topic can't be empty.")
     @Length(min = 5, max = 50, message = "A topic length has to be from 5 to 50 symbols.")
-    @Pattern(regexp = "[^<>]", message = "A topic can't include symbols '<' and '>'.")
-    private String topic;
+    @Pattern(regexp = "[^<>].*", message = "A topic can't include symbols '<' and '>'.")
+    private String eventTopic;
     @NotEmpty(message = "Description can't be empty.")
     @Length(min = 20, max = 200, message = "Description length has to be from 20 to 200 symbols.")
-    @Pattern(regexp = "[^<>]", message = "Description can't include symbols '<' and '>'.")
-    private String description;
-    @NotEmpty(message = "An event has to have at least one organizer.")
-    private List<OrganizerDto> organizers;
+    @Pattern(regexp = "[^<>].*", message = "Description can't include symbols '<' and '>'.")
+    private String eventDescription;
+    @NotNull(message = "An event has to have an organizer.")
+    private OrganizerDto organizer;
     @NotEmpty(message = "An event has to have a date.")
-    @Pattern(regexp = "\\d{2}-\\d{2}\\s\\d{2}\\.\\d{2}\\.\\d{4}", message = "Date has to be in follow format: HH-mm dd.MM.yyyy.")
+    @Pattern(regexp = "\\d{2}-\\d{2}\\s\\d{2}-\\d{2}-\\d{4}", message = "Date has to be in follow format: HH-mm dd-MM-yyyy.")
     private String eventDate;
     @NotNull(message = "En event has to have an address.")
-    private Address address;
+    private AddressDto address;
 
     public EventDto() {
-        organizers = new ArrayList<>();
     }
 
-    public EventDto(String topic, String description, List<OrganizerDto> organizers, String eventDate, Address address) {
-        this.topic = topic;
-        this.description = description;
-        this.organizers = organizers;
+    public EventDto(String eventTopic, String eventDescription, OrganizerDto organizer, String eventDate, AddressDto address) {
+        this.eventTopic = eventTopic;
+        this.eventDescription = eventDescription;
+        this.organizer = organizer;
         this.eventDate = eventDate;
         this.address = address;
     }
 
-    public String getTopic() {
-        return topic;
+    public String getEventTopic() {
+        return eventTopic;
     }
 
-    public void setTopic(String topic) {
-        this.topic = topic;
+    public void setEventTopic(String eventTopic) {
+        this.eventTopic = eventTopic;
     }
 
-    public String getDescription() {
-        return description;
+    public String getEventDescription() {
+        return eventDescription;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
     }
 
-    public List<OrganizerDto> getOrganizers() {
-        return organizers;
+    public OrganizerDto getOrganizer() {
+        return organizer;
     }
 
-    public void setOrganizers(List<OrganizerDto> organizers) {
-        this.organizers = organizers;
+    public void setOrganizer(OrganizerDto organizer) {
+        this.organizer = organizer;
     }
 
     public String getEventDate() {
@@ -70,11 +66,28 @@ public class EventDto {
         this.eventDate = eventDate;
     }
 
-    public Address getAddress() {
+    public AddressDto getAddress() {
         return address;
     }
 
-    public void setAddress(Address address) {
+    public void setAddress(AddressDto address) {
         this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuffer("Event{")
+                .append("topic='")
+                .append(eventTopic)
+                .append("', description='")
+                .append(eventDescription)
+                .append("', organizer=")
+                .append(organizer)
+                .append("', date='")
+                .append(eventDate)
+                .append("', address=")
+                .append(address)
+                .append('}')
+                .toString();
     }
 }
