4837dd1d2134a82b0b58be99f19f1f4f0e7fb9d9
ChaikovskiyV <chaikovskiy.va@mail.ru>
2023-02-06 00:27:36 +0300

    Add fields and filled BeforeAll and BeforeEach methods.

--- src/test/java/com/example/eventservice/controller/EventControllerIntegrationTest.java
+++ src/test/java/com/example/eventservice/controller/EventControllerIntegrationTest.java
@@ -1,22 +1,36 @@
 package com.example.eventservice.controller;
 
 import com.example.eventservice.EventServiceApplication;
+import com.example.eventservice.model.entity.Event;
 import com.example.eventservice.testconfig.TestConfiguration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(classes = EventServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Import(TestConfiguration.class)
 class EventControllerIntegrationTest {
+    public static final String  LOCALHOST = "http://localhost:";
+    public static final String URI = "/api/v1/events";
+    public static final String EVENT_TOPIC_PARAM = "eventTopic=";
+    public static final String EVENT_DATE_PARAM = "eventDate=";
+    public static final String ORGANIZER_PARAM = "organizer";
+    public static final String SORT_PARAM = "sort=";
+
+    private String url;
+    private String eventTopic;
+    private String eventDate;
+    private String organizer;
+    private String sort;
+    private Event event;
+    private List<Event> eventList;
 
     @LocalServerPort
     private int port;
@@ -24,12 +38,22 @@ class EventControllerIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    void init() {
+        url = new StringBuilder(LOCALHOST)
+                .append(port)
+                .append(URI)
+                .toString();
+        eventTopic = "Open IT conference";
+        eventDate = "10-03-2022";
+        organizer = "Bestsoft Ltd";
+        sort = "eventTopic";
     }
 
-    @AfterEach
-    void tearDown() {
+    @BeforeEach
+    void setUp() {
+        event = null;
+        eventList = List.of();
     }
 
     @Test
