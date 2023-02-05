4adb09a976e87b4dcfea6d7c653275c686c25866
ChaikovskiyV <chaikovskiy.va@mail.ru>
2023-02-06 00:08:05 +0300

    Add Javadoc.

--- src/test/java/com/example/eventservice/testconfig/TestConfiguration.java
+++ src/test/java/com/example/eventservice/testconfig/TestConfiguration.java
@@ -9,12 +9,24 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
+/**
+ * @author Viktar Chaikouski
+ * <p>
+ * The type TestConfig.
+ * <p>
+ * Includes the configuration for integration tests.
+ */
 @Configuration
 @PropertySource("classpath:/application-test.properties")
 public class TestConfiguration {
     private static final String TEST_SCHEMA = "classpath:/testdb/testdbschema.sql";
     private static final String TEST_DATA = "classpath:/testdb/testdbdata.sql";
 
+    /**
+     * Returns a dataSource bean.
+     *
+     * @return the DataSource bean
+     */
     @Bean
     public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
@@ -23,6 +35,11 @@ public class TestConfiguration {
                 .build();
     }
 
+    /**
+     * Returns a jdbcTemplate bean.
+     *
+     * @return the JdbcTemplate bean
+     */
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
