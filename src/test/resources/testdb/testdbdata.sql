5e2368bbf6b5e76c57cd4b6ef72b000b1a25216e
ChaikovskiyV <chaikovskiy.va@mail.ru>
2023-02-20 00:09:38 +0300

    Update events and organizers data

--- src/test/resources/testdb/testdbdata.sql
+++ src/test/resources/testdb/testdbdata.sql
@@ -5,10 +5,10 @@ VALUES (1, 'Minsk', 'Gikalo', 5),
 
 INSERT INTO organizers (id, organizer_name, organizer_email, organizer_telephone_number)
 VALUES (4, 'First', 'first@first.com', '+375446116465'),
-       (5, 'Second', 'second@first.com', '+375446116466'),
-       (6, 'Third', 'third@first.com', '+375446116467');
+       (5, 'Second', 'second@second.com', '+375446116466'),
+       (6, 'Third', 'third@third.com', '+375446116467');
 
 INSERT INTO events (id, event_topic, event_description, event_date, organizer_id, address_id)
-VALUES (7, 'How to be a programmer', 'About programming best practice', '2023-03-10', 4, 1),
-       (8, 'How to be a doctor', 'About medicine best practice', '2023-04-10', 5, 3),
-       (9, 'How to be a farmer', 'About growing plant best practice', '2023-03-10', 6, 2);
+VALUES (7, 'How to be a programmer', 'About programming best practice', '2023-03-10 14:10', 4, 1),
+       (8, 'How to be a doctor', 'About medicine best practice', '2023-04-10 15:10', 5, 3),
+       (9, 'How to be a farmer', 'About growing plant best practice', '2023-03-10 16:10', 6, 2);
